d70578619c0bfddd3f96332d0e84e17c217db29c
studyHooligen <2373180028@qq.com>
2020-06-14 15:42:30 +0800

    update PID
    修正了PID算法的标准性

--- PID/PID.cpp
+++ PID/PID.cpp
@@ -19,6 +19,12 @@ PID<T>::PID(T P,T I,T D,T outMax,T outMin)
 }
 
 template<typename T>
+bool PID<T>::PID_setTarget(T target){
+    target_ = target;
+    return true;
+}
+
+template<typename T>
 bool PID<T>::PID_setParam(T P,T I,T D){
     this->K_p = P;
     this->K_i = I;
@@ -27,52 +33,41 @@ bool PID<T>::PID_setParam(T P,T I,T D){
 }
 
 template<typename T>
-bool PID<T>::PID_setTarget(T target){
-    target_ = target;
+bool PID<T>::PID_setThreshold(T outMin,T outMax){
+    max_ = outMax;
+    min_ = outMin;
     return true;
 }
 
-#ifndef _use_incremental_PID_
-
 template<typename T>
-T PID<T>::PID_update(T status){
-    T errNow = target_ - status;  //目前误差值
-
-    error_integral += errNow;   //误差积分
-    T result = 
-        K_p * errNow +              //比例P
-        K_i * error_integral +      //积分I
-        K_d * (errNow - error_Pre); //微分D
-
-    if(result > max_) result = max_;    //输出限幅
-    else if(result < min_) result = min_;
-    
-    error_Pre = errNow; //误差记忆
-    return result;
-}
-
-#else _use_incremental_PID_
-
-template<typename T>
-T PID<T>::PID_update(T status){
+T PID<T>::PID_updateDelta(T status){
     T errNow = target_ - status;  //目前误差值
 
     T delta =   //输出增量
         K_p * (errNow - error_Pre) +    //比例P
         K_i * errNow +                  //积分I
         K_d * (errNow - 2*error_Pre + error_PPre);  //微分D
-    
-    T result = output_pre + delta;  //输出值
 
-    if(result>max_) result = max_;  //输出限幅
-    else if(result<min_) result = min_;
+    if(delta>max_) delta = max_;  //输出限幅
+    else if(delta<min_) delta = min_;
 
-    output_pre = result;    //状态存储
     error_PPre = error_Pre; //误差记忆
     error_Pre = errNow;
 
-    return result;  //返回结果
+    return delta;  //返回结果
+}
+
+#ifdef PID_use_integral
+
+template<typename T>
+T PID<T>::PID_updateStatus(T status){
+
+    output_pre += this->PID_updateDelta(status);
+    
+    if(output_pre>max_) output_pre = max_;  //输出限幅
+    else if(output_pre<min_) output_pre = min_;
+
+    return output_pre;
 }
 
 #endif
-
--- PID/PID.hpp
+++ PID/PID.hpp
@@ -13,7 +13,7 @@
 #ifndef _PID_H_
 #define _PID_H_
 
-#define _use_incremental_PID_ //使用增量式PID
+//#define PID_use_integral  //若使用位置式PID则开启该预编译选项
 
 template<typename T>
 class PID
@@ -26,13 +26,12 @@ private:
     T K_d = 0;  //微分增益
 
     T target_ = 0;   //输出目标值
-    T error_Pre = 0;    //上一次误差
 
-#ifdef _use_incremental_PID_
+    T error_Pre = 0;    //上一次误差
     T error_PPre = 0;   //上上次误差
+
+#ifdef PID_use_integral
     T output_pre = 0;   //上次输出值
-#else
-    T error_integral = 0;   //误差积分值
 #endif
 
     T max_ = 65536;  //PID输出最大值
@@ -53,13 +52,20 @@ public:
      *      I：积分系数
      *      D：微分系数
     */
-    PID(T P,T I,T D,T outMax,T outMin);
+    PID(T P,T I,T D,T outMin,T outMax);
     
     /* 概要：默认析构函数
      * 入参：无
     */
     ~PID();
 
+    /* 概要：改变PID实例参数
+     * 入参：
+     *      target：目标值
+     * 回参：参数设置成功或失败
+    */
+    bool PID_setTarget(T target);
+    
     /* 概要：改变PID实例参数
      * 入参：
      *      P: 比例系数
@@ -69,21 +75,26 @@ public:
     */
     bool PID_setParam(T P,T I,T D);
     
-    /* 概要：改变PID实例参数
+    /* 概要：改变PID实例限幅值
      * 入参：
-     *      P: 比例系数
-     *      I：积分系数
-     *      D：微分系数
+     *      target：目标值
      * 回参：参数设置成功或失败
     */
-    bool PID_setTarget(T target);
+    bool PID_setThreshold(T outMin,T outMax);
 
+    /* 概要：求取PID实例增量值
+     * 入参：
+     *      status：目标状态测量值
+     * 回参：增量值
+    */
+    T PID_updateDelta(T status);
+    
     /* 概要：求取PID实例输出值
      * 入参：
-     *      status：测量目标输出值
-     * 回参：控制器输出值
+     *      status：目标状态测量值
+     * 回参：状态值
     */
-    T PID_update(T status);
+    T PID_updateStatus(T status);
     
     /* 概要：获取P参数
      * 入参：无
